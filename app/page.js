9390b3f02c343f74d84d2457fe278393e1ec2041
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-26 20:41:47 +0300

    import components to page

--- app/page.js
+++ app/page.js
@@ -1,21 +1,15 @@
-import Image from "next/image";
-import Sidebar from "./components/layout/Sidebar";
-import TopNavBar from "./components/layout/TopNavBar";
-import MainContent from "./components/layout/MainContent";
+import TopNavBar from './components/layout/TopNavBar'
+import Sidebar from './components/layout/Sidebar'
+import MainContent from './components/layout/MainContent'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Jesus did it again!</h1>
-    <div className="flex">
+    <div className="flex h-screen">
       <Sidebar />
       <div className="flex flex-col flex-grow">
         <TopNavBar />
-        <MainContent>
-          <h1>Welcome to the Next.js Dev Dashboard</h1>
-          {/* Add more content here */}
-        </MainContent>
+        <MainContent />
       </div>
     </div>
-  );
+  )
 }
