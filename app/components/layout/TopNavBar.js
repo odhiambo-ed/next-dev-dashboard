f978287c2c6181528595e14d6c06c16be0912e7b
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-26 20:44:12 +0300

    add top nav bar components

---
+++ app/components/layout/TopNavBar.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+
+export default function TopNavBar() {
+  return (
+    <nav className="bg-white dark:bg-gray-800 shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Image
+              src="/images/profile-picture.jpg"
+              alt="Profile Picture"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            <span className="ml-3 text-xl font-semibold text-gray-800 dark:text-white">
+              Your Name
+            </span>
+          </div>
+          {/* Add more navigation items or controls here */}
+        </div>
+      </div>
+    </nav>
+  );
+}
