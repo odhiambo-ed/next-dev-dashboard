a03f011c4c19c4362e361d76fe76088fdafa0761
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-26 20:35:43 +0300

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      // You can add custom colors here later
     },
   },
   plugins: [],
+  darkMode: 'class', // This enables dark mode
 };
