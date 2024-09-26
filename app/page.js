d67f3873cc932606bb7533e50ff9dbf52df6fa20
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-26 20:32:38 +0300

    add folder structure and files

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import Sidebar from "./components/layout/Sidebar";
+import TopNavBar from "./components/layout/TopNavBar";
+import MainContent from "./components/layout/MainContent";
 
 export default function Home() {
   return (
     <div>
       <h1>Jesus did it again!</h1>
+    <div className="flex">
+      <Sidebar />
+      <div className="flex flex-col flex-grow">
+        <TopNavBar />
+        <MainContent>
+          <h1>Welcome to the Next.js Dev Dashboard</h1>
+          {/* Add more content here */}
+        </MainContent>
+      </div>
     </div>
   );
 }
