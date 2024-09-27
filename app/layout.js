1232fdf2618f581998a3aa1811f67605eccc689d
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-27 18:40:33 +0300

    Implement basic layout structure with TopNavBar, Sidebar, and MainContent

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,6 @@
-import localFont from "next/font/local";
 import './globals.css'
+import '@/styles/fonts.css';
 import { ThemeProvider } from './contexts/ThemeContext';
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: 'Your Name - Portfolio Dashboard',
@@ -23,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <ThemeProvider>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className="font-['Playwrite_NG_Modern',_sans-serif] antialiased">
         {children}
       </body>
       </ThemeProvider>
